ebaa1038059fb0273a544be330ff3a9e4e126711
DeepeshP04 <pratapdeepesh4@gmail.com>
2024-11-16 13:38:16 +0530

    Create login page

--- app/views.py
+++ app/views.py
@@ -5,3 +5,11 @@ views = Blueprint("views", import_name=__name__, url_prefix="/")
 @views.route("/")
 def index():
     return render_template("index.html") 
+
+@views.route("/login")
+def login():
+    return render_template("login.html")
+
+@views.route("/signup")
+def signup():
+    return render_template("signup.html")
