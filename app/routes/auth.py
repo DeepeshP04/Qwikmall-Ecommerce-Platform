2e5ad2fe8aea4a3dbc52cf99debc865efd00e629
DeepeshP04 <pratapdeepesh4@gmail.com>
2025-04-20 01:29:45 +0530

    Implement login/send_code and signup/send_code in auth.py

--- app/routes/auth.py
+++ app/routes/auth.py
@@ -1,3 +1,31 @@
 from flask import Blueprint
 
 auth_bp = Blueprint("auth", __name__, url_prefix="/auth")
+
+# signup
+@auth_bp.route("/signup/send-code", methods=["POST"])
+def send_code(phone, username):
+    from app.models import User
+    if phone and username:
+        user = User.query.filter_by(phone=phone).first()
+        if user:
+            return {"success": False, "message": "User already exist. Please login."}
+        elif not user:
+            # Generate a verification code
+            # Store code in redis with associated phone number and set expiry time of 5 minutes
+            # Send verification code to the phone number using twilio
+            return {"success": True, "message": "Verification code sent. Please verify to signup."}
+        
+# login
+@auth_bp.route("/login/send-code", methods=["POST"])
+def send_code(phone):
+    from app.models import User
+    if phone:
+        user = User.query.filter_by(phone=phone).first()
+        if user:
+            # Generate a verification code
+            # Store code in redis with associated phone number and set expiry time of 5 minutes
+            # Send verification code to the phone number using twilio
+            return {"success": True, "message": "Verification code sent. Please verify to login."}
+        elif not user:
+            return {"success": False, "message": "User does not exist. Please signup."}
