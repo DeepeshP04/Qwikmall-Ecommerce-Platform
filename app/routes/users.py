1ddbb0150e86eb07e1c71a7d2d33ef150a5f21ad
DeepeshP04 <pratapdeepesh4@gmail.com>
2025-04-22 00:17:38 +0530

    Implement get_user route in users

--- app/routes/users.py
+++ app/routes/users.py
@@ -1,3 +1,14 @@
-from flask import Blueprint
+from flask import Blueprint, jsonify
+from app.models import User
 
 user_bp = Blueprint("users", __name__, url_prefix="/users")
+
+# Get a user by id
+@user_bp.route("/<int:user_id>", methods=["GET"])
+def get_user(user_id):
+    user = User.query.get(user_id)
+    
+    if not user:
+        return jsonify({"success": False, "message": "User does not exist"}), 404
+    
+    return jsonify({"success": True, "user": user.to_dict()}), 200
