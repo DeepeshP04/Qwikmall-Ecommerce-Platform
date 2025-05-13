8f0dd8217018fd9f1e53cc4ca94dd08ee7ba6d62
DeepeshP04 <pratapdeepesh4@gmail.com>
2025-05-13 17:45:36 +0530

    Implement create order api

--- app/routes/orders.py
+++ app/routes/orders.py
@@ -1,5 +1,6 @@
-from flask import Blueprint, session, jsonify
-from app.models import Order
+from flask import Blueprint, session, jsonify, request
+from app.models import Order, Product, Address
+from app import db
 
 order_bp = Blueprint("orders", __name__, url_prefix="/orders")
 
@@ -28,5 +29,36 @@ def get_order_by_id(order_id):
     return jsonify(order.to_dict())
 
 # Create a new order
+@order_bp.route("/", methods={"POST"})
+def create_order():
+    data = request.get_json()
+    product_id = data.get("product_id")
+    quantity = data.get("quantity")
+    
+    user_id = session.get("user_id")
+    user_address = Address.query.filter_by(user_id)
+    
+    product = Product.query.get(product_id)
+    
+    if not product:
+        return jsonify({"success": False, "message": "Product not found"}), 404
+    
+    if quantity <= 0:
+        return jsonify({"success": False, "message": "Quantity must be greater than 0."}), 400
+    
+    total_price = product.price * quantity
+    
+    new_order = Order(
+        user_id = user_id,
+        product_id = product_id,
+        quantity = quantity,
+        total_price = total_price,
+        address = user_address.full_address
+    )
+    
+    db.session.add(new_order)
+    db.session.commit() 
+    
+    return jsonify({"success": True, "message": "Order created successfully"}), 201   
 
 # Update an order
