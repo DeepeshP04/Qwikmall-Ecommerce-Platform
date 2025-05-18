bf6f2b96d2bcf2127ecb75f7599b2c204308ff2d
DeepeshP04 <pratapdeepesh4@gmail.com>
2025-05-18 12:58:43 +0530

    Create payment model for payment related data

--- app/models.py
+++ app/models.py
@@ -118,3 +118,17 @@ class CartItem(db.Model):
             "price": self.price,
             "total_price": self.total_price
         }
+        
+class Payment(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    order_id = db.Column(db.Integer, db.ForeignKey('order.id'), nullable=False)
+    amount = db.Column(db.Float, nullable=False)
+    status = db.Column(db.String(20), nullable=False, default="Pending")
+    method = db.Column(db.String(20), nullable=False)
+    payment_gateway = db.Column(db.String(50), nullable=True)
+    created_at = db.Column(db.DateTime, default=db.func.now())
+    updated_at = db.Column(db.DateTime, default=db.func.now(), onupdate=db.func.now())
+    
+    user = db.relationship("User", backref="payments")
+    order = db.relationship("Order", backref="payment")
