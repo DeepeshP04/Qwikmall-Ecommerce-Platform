063b4f2d503308c163e0b0e70d02458505a62f35
DeepeshP04 <pratapdeepesh4@gmail.com>
2025-05-22 23:20:07 +0530

    Add icons from font-awesome

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,16 +1,30 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCartShopping, faMagnifyingGlass, faUser, faAngleDown } from '@fortawesome/free-solid-svg-icons'
+import { faSellcast } from '@fortawesome/free-brands-svg-icons'
+
 function Navbar (){
     return (
         <div className="navbar">
             <div className="brand">
-                <p>Qwikmall</p>
+                <a>Qwikmall</a>
             </div>
             <form className="search-bar" method="GET">
                 <input type="search" name="query" placeholder="Search for products..."/>
-                <button type="submit"></button>
+                <button type="submit"><FontAwesomeIcon icon={faMagnifyingGlass} /></button>
             </form>
-            <div className="login-space"></div>
-            <div className="cart"></div>
-            <div className="seller"></div>
+            <div className="login-space">
+                <FontAwesomeIcon icon={faUser} />
+                <p>Login</p>
+                <FontAwesomeIcon icon={faAngleDown} />
+            </div>
+            <div className="cart">
+                <FontAwesomeIcon icon={faCartShopping} />
+                <p>Cart</p>
+            </div>
+            <div className="seller">
+                <FontAwesomeIcon icon={faSellcast} />
+                <p>Become a Seller</p>
+            </div>
         </div>
     )
 }
