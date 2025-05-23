62106e672133cdb37f7ce781c9a287370f1e2e71
DeepeshP04 <pratapdeepesh4@gmail.com>
2025-05-23 15:27:20 +0530

    Implement ProductDetails component for displaying a product's details and style it

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,14 @@
 import './App.css'
-import Footer from './components/Footer/Footer'
-import Navbar from './components/Header/Navbar'
+import Footer from './components/footer/Footer'
+import Navbar from './components/header/Navbar'
+import ProductDetails from './components/main/ProductDetails'
 
 function App() {
 
   return (
     <>
       <Navbar></Navbar>
+      <ProductDetails></ProductDetails>
       <Footer></Footer>
     </>
   )
---
+++ frontend/src/components/main/ProductDetails.jsx
@@ -0,0 +1,13 @@
+import './ProductDetails.css'
+
+function ProductDetails () {
+    return (
+        <div className="product-details">
+            <img className="product-img" src="" alt="Product Image"/>
+            <p className="product-name">Laptop</p>
+            <p className="product-price">50000</p>
+        </div>
+    )
+}
+
+export default ProductDetails;
