4ae024a2e3739bb6bf10ae1a5901be0af34f82f5
DeepeshP04 <pratapdeepesh4@gmail.com>
2025-05-24 13:15:31 +0530

    Create AuthComponent for signup and login

---
+++ frontend/src/components/auth/signup/AuthComponent.jsx
@@ -0,0 +1,29 @@
+function AuthComponent() {
+    return (
+        <div className="auth-container">
+            <div className="left-box">
+                <p>Looks like you're new here!</p>
+                <p>Sign up with your mobile number to get started</p>
+            </div>
+            <div className="right-box">
+                <div className="form-section">
+                    <div className="input-section">
+                        <input id="phone" type="number" placeholder="Enter Phone Number"></input>
+                    </div>
+                    <div className="terms-section">
+                        <input id="terms-checkbox" type="checkbox"></input>
+                        <p>By continuing, you agree to QwikMall's Terms of Use and Privacy Policy.</p>
+                    </div>
+                    <div className="button-section">
+                        <button id="submit-btn" type="submit">Sign Up</button>
+                    </div>
+                </div>
+            </div>
+            <div className="go-to-login-section">
+                <a href="">Existing User? Log in</a>
+            </div>
+        </div>
+    )
+}
+
+export default AuthComponent;
