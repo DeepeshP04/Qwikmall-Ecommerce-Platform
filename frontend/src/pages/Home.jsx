2bd82819d5ee4e7379d04445d02bc971cc647c04
DeepeshP04 <pratapdeepesh4@gmail.com>
2025-05-25 14:27:55 +0530

    Create Home page using Navbar, Footer, Main and Route for '/' path

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,15 @@
+import Navbar from '../components/header/Navbar'
+import Main from '../components/main/Main'
+import Footer from '../components/footer/Footer'
+
+function Home() {
+    return (
+        <>
+            <Navbar></Navbar>
+            <Main></Main>
+            <Footer></Footer>
+        </>
+    )
+}
+
+export default Home;
