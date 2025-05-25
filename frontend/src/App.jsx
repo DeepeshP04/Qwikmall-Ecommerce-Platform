fa14c5913ccf18883563b3d4c3ca9e1be8700fc2
DeepeshP04 <pratapdeepesh4@gmail.com>
2025-05-25 14:20:36 +0530

    Create home, login, signup pages for different paths

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,20 +1,9 @@
 import './App.css'
-import Login from './components/auth/login/Login'
-import Signup from './components/auth/signup/Signup'
-import Footer from './components/footer/Footer'
-import Navbar from './components/header/Navbar'
-import Main from './components/main/Main'
 
 function App() {
 
   return (
-    <>
-      <Navbar></Navbar>
-      <Main></Main>
-      <Signup></Signup>
-      <Login></Login>
-      <Footer></Footer>
-    </>
+  ""
   )
 }
 
