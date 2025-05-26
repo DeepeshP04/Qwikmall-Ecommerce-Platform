11383d899640e83b25b19f6e994fa42551641571
DeepeshP04 <pratapdeepesh4@gmail.com>
2025-05-26 21:09:34 +0530

    Update FilterOption component to take props and display data uing prop

--- frontend/src/components/products/FilterOption.jsx
+++ frontend/src/components/products/FilterOption.jsx
@@ -2,22 +2,20 @@ import './FilterOption.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faAngleDown } from '@fortawesome/free-solid-svg-icons';
 
-function FilterOption () {
+function FilterOption ({title, options, filterId}) {
     return (
         <div className='filter-block'>
             <div className="filter-header">
-                <h4 className="filter-title">Size</h4>
+                <h4 className="filter-title">{title}</h4>
                 <FontAwesomeIcon icon={faAngleDown} className='toggle-icon'/>
             </div>
             <div className="filter-content">
-                <div className="filter-option-item">
-                    <input type="checkbox" id="size-2xl"></input>
-                    <label htmlFor='size-2xl'>2XL</label>
-                </div>
-                <div className="filter-option-item">
-                    <input type="checkbox" id="size-2xl"></input>
-                    <label htmlFor='size-2xl'>2XL</label>
-                </div>
+                {options.map((option, idx) => (
+                    <div className="filter-option-item" key={`${filterId}-${idx}`}>
+                        <input type="checkbox" id={`${filterId}-${option}`}></input>
+                        <label htmlFor={`${filterId}-${option}`}>{option}</label>
+                    </div>
+                ))}
             </div>
         </div>
     )
