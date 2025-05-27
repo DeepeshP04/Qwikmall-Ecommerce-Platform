80c02c91ecfee8a5a561f48ebfaabf38804a6ce8
DeepeshP04 <pratapdeepesh4@gmail.com>
2025-05-27 15:45:59 +0530

    Remove div for filterpanel, give styling in CategoryAllProducts

--- frontend/src/components/products/CategoryAllProducts.jsx
+++ frontend/src/components/products/CategoryAllProducts.jsx
@@ -1,25 +1,16 @@
-import FilterPanel from './FilterPanel';
 import ProductGrid from './ProductGrid';
 import './CategoryAllProducts.css'
 
-function CategoryAllProducts () {
-    const products = [
-        {id: 1, name: "Laptop", price: "₹50000"},
-        {id: 2, name: "Laptop", price: "₹50000"},
-        {id: 3, name: "Laptop", price: "₹50000"},
-        {id: 4, name: "Laptop", price: "₹50000"},
-        {id: 5, name: "Laptop", price: "₹50000"},
-    ]
+function CategoryAllProducts ({categoryName, products}) {
     return (
         <div className="category-all-products">
-            <div className="filter-products-space">
-                <FilterPanel></FilterPanel>
-            </div>
-            <div className="main-content">
-                <h2 className="category-title">Electronics</h2>
-                <div className="products-container">
-                    <ProductGrid products={products}></ProductGrid>
-                </div>
+            {/* Add filter panel component for displaying different filter options for products */}
+            {/* <div className="filter-products-space">
+                <FilterPanel filters={filters}></FilterPanel>
+            </div>  */}
+            <h2 className="category-title">{categoryName}</h2>
+            <div className="products-container">
+                <ProductGrid products={products}></ProductGrid>
             </div>
         </div>
     )
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -4,12 +4,22 @@ import Footer from '../components/footer/Footer'
 import CategoryAllProducts from '../components/products/CategoryAllProducts';
 
 function Home() {
+    const products = [
+        {id: 1, name: "Laptop", price: "₹50000"},
+        {id: 2, name: "Laptop", price: "₹50000"},
+        {id: 3, name: "Laptop", price: "₹50000"},
+        {id: 4, name: "Laptop", price: "₹50000"},
+        {id: 5, name: "Laptop", price: "₹50000"},
+        {id: 5, name: "Laptop", price: "₹50000"},
+        {id: 5, name: "Laptop", price: "₹50000"},
+    ]
+
     return (
         <>
             <Navbar></Navbar>
             <Main></Main>
             <Footer></Footer>
-            <CategoryAllProducts></CategoryAllProducts>
+            <CategoryAllProducts categoryName="Electronics" products={products}></CategoryAllProducts>
         </>
     )
 }
