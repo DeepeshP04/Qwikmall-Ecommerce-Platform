7d8709617f020c7d793cb3b77ddf6d4c52662499
DeepeshP04 <pratapdeepesh4@gmail.com>
2025-06-06 16:05:48 +0530

    Update RecommendedProdcuts component to fetch and display data from api

--- frontend/src/components/products/RecommendedCategoryProducts.jsx
+++ frontend/src/components/products/RecommendedCategoryProducts.jsx
@@ -3,8 +3,17 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faAngleRight } from '@fortawesome/free-solid-svg-icons'
 import ProductGrid from './ProductGrid'
 import { Link } from 'react-router-dom'
+import { useEffect, useState } from 'react'
 
 function RecommendedCategoryProducts () {
+    const [categoryProducts, setCategoryProducts] = useState([]);
+
+    useEffect(() => {
+        fetch("/products/recommended-products")
+        .then(res => setCategoryProducts(res.data.categories))
+        .catch(err => console.error(err))
+    }, [])
+
     const products = [
         {id: 1, name: "Laptop", price: "₹50000"},
         {id: 2, name: "Laptop", price: "₹50000"},
@@ -15,13 +24,17 @@ function RecommendedCategoryProducts () {
 
     return (
         <div className="recommended-category-products">
-            <div className="category">
-                <p className='category-name'>Electronics</p>
-                <Link to="category/categoryName" className='category-all-products-btn'><FontAwesomeIcon icon={faAngleRight} className='right-arrow' /></Link>
-            </div>
-            <div className="recommeded-products-container">
-                <ProductGrid products={products}></ProductGrid>
-            </div>
+            {categoryProducts.map((category => 
+                <div key={category.id}>
+                    <div className='category'>
+                        <p className='category-name'>{category.name}</p>
+                        <Link to={`category/${category.name}`} className='category-all-products-btn'><FontAwesomeIcon icon={faAngleRight} className='right-arrow' /></Link>
+                    </div>
+                    <div className="recommeded-products-container">
+                        <ProductGrid products={category.products}></ProductGrid>
+                    </div>
+                </div>
+            ))}
         </div>
     )
 }
