68ce01e50aa1c0627f4166f92890d8f1f75f5640
DeepeshP04 <pratapdeepesh4@gmail.com>
2025-06-11 17:04:06 +0530

    Update get_products_by_category route to include category_name and use category_name in CategoryProductsPage

--- frontend/src/pages/CategoryProductsPage.jsx
+++ frontend/src/pages/CategoryProductsPage.jsx
@@ -1,4 +1,4 @@
-import { useParams } from "react-router-dom";
+import { data, useParams } from "react-router-dom";
 import Footer from "../components/footer/Footer";
 import Navbar from "../components/header/Navbar";
 import CategoryAllProducts from "../components/products/CategoryAllProducts";
@@ -6,19 +6,19 @@ import { useEffect, useState } from "react";
 
 function CategoryProductsPage () {
     const {categoryId} = useParams()
-    const [categoryProducts, setCategoryProducts] = useState([])
+    const [categoryProducts, setCategoryProducts] = useState({category_name: "", products: []})
 
     useEffect(() => {
         fetch(`http://localhost:5000/products/category/${categoryId}`)
         .then(res => res.json())
-        .then(data => setCategoryProducts(data.products))
+        .then(data => setCategoryProducts(data))
         .catch(err => console.log("Failed to fetch products", err))
-    }, [])
+    }, [categoryId])
 
     return (
         <>
             <Navbar></Navbar>
-            <CategoryAllProducts categoryName={categoryId} products={categoryProducts}></CategoryAllProducts>
+            <CategoryAllProducts categoryName={categoryProducts.category_name} products={categoryProducts.products}></CategoryAllProducts>
             <Footer></Footer>
         </>
     )
