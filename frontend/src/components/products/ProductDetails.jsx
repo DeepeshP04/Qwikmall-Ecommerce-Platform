eedb7ba0aaee38509c99ac1d5e01b3b9d76126c9
DeepeshP04 <pratapdeepesh4@gmail.com>
2025-06-11 13:23:30 +0530

    Create ProductPage for displaying product and options for buy or add to cart

--- frontend/src/components/products/ProductDetails.jsx
+++ frontend/src/components/products/ProductDetails.jsx
@@ -1,18 +1,20 @@
 function ProductDetails({ product }) {
-    <div className="product-details-section">
-        <div className="left">
-            <img id="product-img" src={product.img_url} alt={product.name}/>
-            <div className="buy-or-add-cart">
-                <button id="buy-btn">Buy Now</button>
-                <button id="add-to-cart-btn">Add to Cart</button>
+    return (
+        <div className="product-details-section">
+            <div className="left">
+                <img id="product-img" src={product.img_url} alt={product.name}/>
+                <div className="buy-or-add-cart">
+                    <button id="buy-btn">Buy Now</button>
+                    <button id="add-to-cart-btn">Add to Cart</button>
+                </div>
+            </div>
+            <div className="right">
+                <h3>{product.name}</h3>
+                <p>{product.price}</p>
+                <p>{product.description}</p>
             </div>
         </div>
-        <div className="right">
-            <h3>{product.name}</h3>
-            <p>{product.price}</p>
-            <p>{product.description}</p>
-        </div>
-    </div>
+    )
 }
 
 export default ProductDetails;
---
+++ frontend/src/pages/ProductPage.jsx
@@ -0,0 +1,18 @@
+import Footer from "../components/footer/Footer";
+import Navbar from "../components/header/Navbar";
+import ProductDetails from "../components/products/ProductDetails";
+
+function ProductPage() {
+    const product = {
+        id: 1, name: "Phone", description: "Phone", image_url: "frontend/public/images/laptop_electronics.jpg", price: 10000
+    }
+    return (
+        <>
+            <Navbar></Navbar>
+            <ProductDetails product={product}></ProductDetails>
+            <Footer></Footer>
+        </>
+    )
+}
+
+export default ProductPage;
