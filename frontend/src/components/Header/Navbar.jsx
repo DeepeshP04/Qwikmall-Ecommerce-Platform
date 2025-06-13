648cacf66cb3c90d94baaab1501e528febca4f5c
DeepeshP04 <pratapdeepesh4@gmail.com>
2025-06-13 13:26:24 +0530

    Implement toggle nav-actions functinality and use useEffect to setMenuOpen to false when window width gets bigger than 850px

--- frontend/src/components/Header/Navbar.jsx
+++ frontend/src/components/Header/Navbar.jsx
@@ -2,14 +2,22 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faCartShopping, faMagnifyingGlass, faUser, faAngleDown } from '@fortawesome/free-solid-svg-icons'
 import { faSellcast } from '@fortawesome/free-brands-svg-icons'
 import './Navbar.css'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import {Link} from 'react-router-dom'
-import Login from '../../pages/Login'
 
 function Navbar (){
 
     const [isMenuOpen, setMenuOpen] = useState(false);
     const toggleMenu = () => setMenuOpen(!isMenuOpen);
+
+    useEffect(() => {
+        const handleResize = () => {
+            if (window.innerWidth > 850) {
+                setMenuOpen(false)
+            }
+        }
+        window.addEventListener("resize", handleResize)
+    }, [])
 
     return (
         <div className="navbar">
@@ -20,7 +28,7 @@ function Navbar (){
                 <input id="search-input" type="search" name="query" placeholder="Search for products..."/>
                 <button id="search-btn" type="submit"><FontAwesomeIcon icon={faMagnifyingGlass} /></button>
             </form>
-            <div className='nav-actions'>
+            <div className="nav-actions">
                 <div className="login-space">
                     <FontAwesomeIcon icon={faUser} />
                     <Link to="/login" className="login">Login</Link>
@@ -36,10 +44,27 @@ function Navbar (){
                     <a className='become-seller'>Become a Seller</a>
                 </div>
             </div>
-            <button className='nav-actions-toggle' onClick={toggleMenu}>☰</button>
-        </div>
 
-        // Implement toggleMenu function to toggle nav-actions in mobile
+            {/* Implement toggleMenu function to toggle nav-actions in mobile */}
+            <button className='nav-actions-toggle' onClick={toggleMenu}>☰</button>
+
+            {isMenuOpen ? (<div className="mobile-nav-actions">
+                <div className="login-space">
+                    <FontAwesomeIcon icon={faUser} />
+                    <Link to="/login" className="login">Login</Link>
+                    <FontAwesomeIcon icon={faAngleDown} />
+                    {/* Implement functionality to click angledown button to choose login or signup */}
+                </div>
+                <div className="cart-space">
+                    <FontAwesomeIcon icon={faCartShopping} />
+                    <a className='cart'>Cart</a>
+                </div>
+                <div className="seller-space">
+                    <FontAwesomeIcon icon={faSellcast} />
+                    <a className='become-seller'>Become a Seller</a>
+                </div>
+            </div>) : ""}
+        </div>
     )
 }
 
