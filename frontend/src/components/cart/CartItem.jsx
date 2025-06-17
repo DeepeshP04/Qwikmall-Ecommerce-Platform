51fed634a774411c4976db9f947089c42c107293
DeepeshP04 <pratapdeepesh4@gmail.com>
2025-06-17 13:50:48 +0530

    Update CartItem component and style component

--- frontend/src/components/cart/CartItem.jsx
+++ frontend/src/components/cart/CartItem.jsx
@@ -6,14 +6,25 @@ function CartItem () {
     }
 
     return (
-        <div className="cart-item">
+        <div className="cart-item-container">
             <div className="item-img-space">
                 <img src={cartItem.imgUrl} alt={cartItem.name}></img>
             </div>
-            <div className="item-details-space">
-                <h3 id="item-name">{cartItem.name}</h3>
-                <p id="item-price">{cartItem.price}</p>
-                <button id="item-remove-btn">Remove</button>
+            <div className='item-content-space'>
+                <div className="item-details-space">
+                    <h3 className="item-name">{cartItem.name}</h3>
+                    <p className="item-price">{cartItem.price}</p>
+                </div>
+                <div className='item-action-space'>
+                    <div className='quantity-control'>
+                        <button className='quantity-decrease-btn'>-</button>
+                        <input className='quantity-input' type='number' value="1" readOnly></input>
+                        <button className='quantity-increase-btn'>+</button>
+                    </div>
+                    <div className='remove-btn-container'>
+                        <button id="item-remove-btn">Remove</button>
+                    </div>
+                </div>
             </div>
         </div>
     )
