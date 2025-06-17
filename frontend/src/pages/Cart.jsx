ccd9ed52d11afdf514a8b3aab8efc8b88d97f2cd
DeepeshP04 <pratapdeepesh4@gmail.com>
2025-06-17 12:37:05 +0530

    Initialize cart page

---
+++ frontend/src/pages/Cart.jsx
@@ -0,0 +1,13 @@
+import Footer from "../components/footer/Footer";
+import Navbar from "../components/header/Navbar";
+
+function Cart () {
+    return (
+        <>
+            <Navbar></Navbar>
+            <Footer></Footer>
+        </>
+    )
+}
+
+export default Cart;
