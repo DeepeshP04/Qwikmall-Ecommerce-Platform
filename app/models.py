edb9c7fc3d89a228cc528775023531847f145290
DeepeshP04 <pratapdeepesh4@gmail.com>
2025-07-06 12:45:09 +0530

    Remove img and stock fields from product model

--- app/models.py
+++ app/models.py
@@ -33,7 +33,6 @@ class Product(db.Model):
 class Category(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(50), nullable=False)
-    description = db.Column(db.String(100), nullable=True)
     products = db.relationship('Product', backref='category', lazy=True)
     
 class Address(db.Model):
